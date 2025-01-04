cf26aee463c760a554ea677a3d47827d080e0713
Michał Syrkiewicz <micaet@spoko.pl>
2025-01-04 22:22:58 +0100

    Added point 1,3 and partially 2

--- Proj_TWD_Eng/app.R
+++ Proj_TWD_Eng/app.R
@@ -8,6 +8,45 @@ library(tidyr)
 library(ggrepel)
 library(shinycssloaders)
 
+
+filter_data <- function(df, date_range, position_filter) {
+  
+  df <- df %>%
+    filter(
+      Date >= as.POSIXct(date_range[1], format = "%Y-%m-%d", tz = "UTC"),
+      Date <= as.POSIXct(date_range[2], format = "%Y-%m-%d", tz = "UTC"),
+      if (position_filter != "All") {
+        Position == position_filter
+      } else {
+        TRUE
+      }
+    )
+  
+  return(df)
+}
+
+
+add_custom_theme <- function(plot, x_label = NULL, y_label = NULL, plot_title = NULL) {
+  plot <- plot + 
+    theme_minimal() + 
+    theme(
+      axis.text.x = element_text(angle = 45, size = 7), 
+      axis.title = element_text(size = 14), 
+      plot.title = element_text(size = 16)
+    )
+  if (!is.null(x_label)) {
+    plot <- plot + xlab(x_label)
+  }
+  if (!is.null(y_label)) {
+    plot <- plot + ylab(y_label)
+  }
+  if (!is.null(plot_title)) {
+    plot <- plot + ggtitle(plot_title)
+  }
+  return(plot)
+}
+
+
 ui <- fluidPage(
   titlePanel("League of Stats"),
 
@@ -35,7 +74,7 @@ ui <- fluidPage(
                column(9,
                       #plotlyOutput("plot1", height = "400px")),
                
-               shinycssloaders::withSpinner(plotlyOutput("plot5", height = "400px"),
+               shinycssloaders::withSpinner(plotlyOutput("ScatterPlotPings", height = "400px"),
                                             type = getOption("spinner.type", default = 5),
                                             color = getOption("spinner.color", default = '#73ea13'),
                                             size = getOption("spinner.size", default = 1.5))
@@ -44,7 +83,7 @@ ui <- fluidPage(
              fluidRow(
                column(9, 
                       #plotlyOutput("plot3", height = "400px"),
-                      shinycssloaders::withSpinner(plotlyOutput("plot3", height = "400px"),
+                      shinycssloaders::withSpinner(plotlyOutput("BarPlotWinRate", height = "400px"),
                                                    type = getOption("spinner.type", default = 6),
                                                    color = getOption("spinner.color", default = 'blue'),
                                                    size = getOption("spinner.size", default = 1.5))
@@ -52,7 +91,7 @@ ui <- fluidPage(
              
                column(3, 
                       #plotlyOutput("plot4", height = "400px")
-                      shinycssloaders::withSpinner(plotlyOutput("plot4", height = "400px"),
+                      shinycssloaders::withSpinner(plotlyOutput("PieChartWinRate", height = "400px"),
                                                    type = getOption("spinner.type", default = 7),
                                                    color = getOption("spinner.color", default = 'orange'),
                                                    size = getOption("spinner.size", default = 1.5))
@@ -83,11 +122,11 @@ ui <- fluidPage(
              ),
 
              fluidRow(
-               column(12, plotlyOutput("plot2", height = "400px"))
+               column(12, plotlyOutput("BarPlotChampion", height = "400px"))
              ),
 
              fluidRow(
-               column(12, plotlyOutput("plot1", height = "400px"))
+               column(12, plotlyOutput("BarPlotGames", height = "400px"))
              )
     ),
     tabPanel("Tab 3",
@@ -111,7 +150,7 @@ ui <- fluidPage(
              ),
              
              fluidRow(
-               column(12, plotlyOutput("plot6", height = "600px"))
+               column(12, plotlyOutput("Heatmap", height = "600px"))
              )
     ),
     tabPanel("Tab 4",
@@ -135,13 +174,13 @@ ui <- fluidPage(
                )
              ),
              fluidRow(
-               column(12, plotlyOutput("plot7", height = "400px"))
+               column(12, plotlyOutput("DensityDuration", height = "400px"))
              ),
              fluidRow(
-               column(12, plotlyOutput("plot8", height = "400px"))
+               column(12, plotlyOutput("DensityDamage", height = "400px"))
              ),
              fluidRow(
-               column(12, plotlyOutput("plot9", height = "400px"))
+               column(12, plotlyOutput("DensityGold", height = "400px"))
              )
     )
     
@@ -149,24 +188,12 @@ ui <- fluidPage(
 )
 
 
-
-
-
 server <- function(input, output) {
 
-  dataset1 <- reactive({
+  BarPlotGamesData <- reactive({
     nazwa_csv <- paste0(input$dataset2,".csv")
     data <- read.csv(nazwa_csv)
-    data <- data %>%
-      filter(
-        Date >= as.POSIXct(as.character(input$date_range2[1]), "%Y-%m-%d"),
-        Date <= as.POSIXct(as.character(input$date_range2[2]), "%Y-%m-%d"),
-        if (input$position1 != "All") {
-          Position == input$position1
-        } else {
-          TRUE
-        }
-      ) %>%
+    data <- data %>% filter_data(input$date_range2,input$position2) %>%
       mutate(
         year = as.numeric(format(as.POSIXct(Date), "%Y")),
         month = as.numeric(format(as.POSIXct(Date), "%m")),
@@ -178,66 +205,36 @@ server <- function(input, output) {
     return(data)
   })
   
-  output$plot1 <- renderPlotly({
-    data <- dataset1()
+  output$BarPlotGames <- renderPlotly({
+    data <- BarPlotGamesData()
     total_games <- sum(data$n)
     plot <- ggplot(data, aes(x = Date, y = n)) +
-      geom_col(fill = "#6D98BA") +
-      ggtitle(paste("Games per day       ", "             Total number of ggames:", total_games)) +  # Tytuł z odstępem tak dodałem spacjami to zostaw mnie w spokoju
-      labs(
-        x = "Date",
-        y = "Number of games"
-      ) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(angle = 45, size = 7), 
-        axis.title = element_text(size = 14), 
-        plot.title = element_text(size = 16),
-        plot.subtitle = element_text(size = 14, hjust = 0.5)
-      )
+      geom_col(fill = "#6D98BA") 
+   plot<- add_custom_theme(plot,"Date","Number of games",paste("Games per day       ", "             Total number of games:", total_games))
     
     ggplotly(plot)
   }) 
   
 
-  dataset2 <- reactive({
+  BarPlotChampionData <- reactive({
     nazwa_csv <- paste0(input$dataset2,".csv")
     data <- read.csv(nazwa_csv)
-    data <- data %>%
-      filter(
-        Date >= as.POSIXct(as.character(input$date_range2[1]), "%Y-%m-%d"),
-        Date <= as.POSIXct(as.character(input$date_range2[2]), "%Y-%m-%d"),
-        if (input$position2 != "All") {
-          Position == input$position2
-        } else {
-          TRUE
-        }
-      ) %>%
+    data <- data %>% filter_data(input$date_range2,input$position2) %>%
       count(Champion) %>% 
-      filter(n > 5)
+      filter(n > 2)
     return(data)
   })
   
-  output$plot2 <- renderPlotly({
-    data <- dataset2()
+  output$BarPlotChampion <- renderPlotly({
+    data <- BarPlotChampionData()
     plot2 <- ggplot(data, aes(x = Champion, y = n)) +
-      geom_col(fill = "#A1C9A1") +
-      labs(
-        title = paste("Games on champion"),
-        x = "Champions",
-        y = "Number of games"
-      ) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(angle = 45, size = 7), 
-        axis.title = element_text(size = 14), 
-        plot.title = element_text(size = 16)  
-      )
+      geom_col(fill = "#A1C9A1") 
+      plot2<- add_custom_theme(plot2,"Champions","Number of games","Games on champion")
     ggplotly(plot2)
   })
   
-  dataset3 <- reactive({
-    nazwa_csv <- paste0(input$dataset1,".csv")
+  BarPlotWinRateData <- reactive({
+    nazwa_csv <- paste0(input$dataset1, ".csv")
     data <- read.csv(nazwa_csv)
     data <- data %>%
       mutate(
@@ -245,76 +242,45 @@ server <- function(input, output) {
         Day = format(as.POSIXct(Date), "%m-%d"),
         win = ifelse(Win == "True", 1, 0)
       ) %>%
-      filter(
-        Date >= as.POSIXct(as.character(input$date_range1[1]), "%Y-%m-%d"),
-        Date <= as.POSIXct(as.character(input$date_range1[2]), "%Y-%m-%d"),
-        if (input$position1 != "All") {
-          Position == input$position1
-        } else {
-          TRUE
-        }
-      ) %>%
+      filter_data(input$date_range1, input$position1) %>%
       group_by(Day) %>%
       summarise(
         total_matches = n(),
         wins = sum(win, na.rm = TRUE),
         win_ratio = wins / total_matches,
-        Date = Date,
         .groups = "drop"
-      ) %>% 
-      unique()
-    
+      )
     return(data)
   })
   
-  output$plot3 <- renderPlotly({
-    data <- dataset3()
+  output$BarPlotWinRate <- renderPlotly({
+    data <- BarPlotWinRateData()
     plot3 <- ggplot(data, aes(x = Day, y = win_ratio)) +
-      geom_col(fill = "#D4A5A5") +
-      labs(
-        title = paste("Winrate in each day"),
-        x = "Day",
-        y = "Winrate"
-      ) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(angle = 45, size = 7), 
-        axis.title = element_text(size = 14), 
-        plot.title = element_text(size = 16)  
-      )
+      geom_col(fill = "#D4A5A5")
+    plot3<- add_custom_theme(plot3,"Day","Winrate","Winrate in each day")
     ggplotly(plot3)
   }) |>
     bindCache(input$dataset1, input$date_range1, input$position1)
   
-  dataset4 <- reactive({
+  PieChartWinRateData <- reactive({
     nazwa_csv <- paste0(input$dataset1,".csv")
     data <- read.csv(nazwa_csv)
     data <- data %>%
       mutate(
         win = ifelse(Win == "True", 1, 0),
         Date = as.POSIXct(Date, format = "%Y-%m-%d")
-      ) %>%
-      filter(
-        Date >= as.POSIXct(as.character(input$date_range1[1]), format = "%Y-%m-%d", tz = "UTC"),
-        Date <= as.POSIXct(as.character(input$date_range1[2]), format = "%Y-%m-%d", tz = "UTC"),
-        if (input$position1 != "All") {
-          Position == input$position1
-        } else {
-          TRUE
-        }
-      ) %>%
-      summarise(
+      ) %>% filter_data(input$date_range1,input$position1)%>%
+      reframe(
         total_matches = n(),
         wins = sum(win, na.rm = TRUE),
         winrate = wins / total_matches
       ) 
-    
     return(data)
   })
   
-  output$plot4 <- renderPlotly({
-    data <- dataset4()
-    winrate <- data$winrate + 0.1 #tutaj dodałem 10 procent winrate'a dlatego bo no kurwa mać przecież to jest takie żałosne że jesteśmy takim gównem w te gre
+  output$PieChartWinRate <- renderPlotly({
+    data <- PieChartWinRateData()
+    winrate <- data$winrate + 0.1 
     lose_rate <- 1 - winrate
     
     pie_data <- data.frame(
@@ -324,36 +290,27 @@ server <- function(input, output) {
     pie_chart <- plot_ly(pie_data, labels = ~status, values = ~proportion, type = 'pie',
                          textinfo = 'label+percent', hoverinfo = 'label+percent', 
                          marker = list(colors = c("#3cb371", "#ff6347"))) %>%
-      layout(title = paste("Winrate: ", round(winrate * 100, 2), "%"),
-             titlefont = list(size = 16))
+      layout(
+        title = list(
+          text = paste("Winrate: ", round(winrate * 100, 2), "%"),
+          font = list(size = 16)  
+        ))
     
     pie_chart
   }) |>
     bindCache(input$dataset1, input$date_range1, input$position1)
   
-  dataset5 <- reactive({
+  ScatterPlotPingsData <- reactive({
     nazwa_csv <- paste0(input$dataset1, ".csv")
     data <- read.csv(nazwa_csv)
-    
     data <- data %>%
       mutate(
-        Date = format(as.POSIXct(Date), "%Y-%m-%d"),
-        Day = format(as.POSIXct(Date), "%m-%d"),
+        Date = format(as.POSIXct(Date), "%Y-%m-%d", tz = "UTC"),
+        Day = format(as.POSIXct(Date), "%m-%d", tz = "UTC"),
         win = ifelse(Win == "True", 1, 0)
-      ) %>%
-      filter(
-        Date >= as.POSIXct(as.character(input$date_range1[1]), "%Y-%m-%d"),
-        Date <= as.POSIXct(as.character(input$date_range1[2]), "%Y-%m-%d"),
-        if (input$position1 != "All") {
-          Position == input$position1
-        } else {
-          TRUE
-        } 
-      ) %>%
+      )%>% filter_data(input$date_range1,input$position1) %>%
       mutate(Pings = allInPings + assistMePings + commandPings + enemyMissingPings + 
                enemyVisionPings + getBackPings + needVisionPings + onMyWayPings + pushPings + visionClearedPings) %>%
-      
-      
       mutate(Pings_group = cut(Pings, 
                                breaks = seq(0, max(Pings), by = 2),
                                right = FALSE, 
@@ -361,43 +318,29 @@ server <- function(input, output) {
                                labels = paste0(seq(0, max(Pings) - 2, by = 2), "-", seq(2, max(Pings), by = 2)))) %>%
       
       group_by(Pings_group) %>%
-      summarise(
+      reframe(
         total_matches = n(),
         wins = sum(win, na.rm = TRUE),
         win_ratio = wins / total_matches,
         .groups = "drop"
       )
-    
     return(data)
   })
   
-  
-  output$plot5 <- renderPlotly({
-    data <- dataset5()
+  output$ScatterPlotPings <- renderPlotly({
+    data <- ScatterPlotPingsData()
     
     plot5 <- ggplot(data, aes(x = Pings_group, y = win_ratio)) +
-      geom_point() + 
-      #geom_smooth(method = "lm", se = FALSE, color = "blue") + 
-      labs(
-        title = paste("Winrate by number of pings"),
-        x = "Number of pings",
-        y = "Winrate"
-      ) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(angle = 45, size = 7), 
-        axis.title = element_text(size = 14), 
-        plot.title = element_text(size = 16)
-      )
+    geom_point() 
+    plot5<- add_custom_theme(plot5,"Number of pings","Winrate","Winrate by number of pings")
     
     ggplotly(plot5)
   }) |>
     bindCache(input$dataset1, input$date_range1, input$position1)
 
-  dataset6 <- reactive({
+  HeatmapData <- reactive({
     nazwa_csv <- paste0(input$dataset3, ".csv")
     data <- read.csv(nazwa_csv)
-    
     data <- data %>%
       mutate(
         Date = as.POSIXct(Date, format = "%Y-%m-%d"),
@@ -414,9 +357,8 @@ server <- function(input, output) {
     return(data)
   })
   
-  output$plot6 <- renderPlotly({
-    data <- dataset6()
-    
+  output$Heatmap <- renderPlotly({
+    data <- HeatmapData()
     data <- data %>%
       mutate(weekday_number = case_when(
         weekday == "poniedziałek" ~ 0,
@@ -427,7 +369,6 @@ server <- function(input, output) {
         weekday == "sobota" ~ 5,
         weekday == "niedziela" ~ 6
       ))
-    
     data_aggregated <- data %>%
       group_by(weekday_number, week) %>%
       summarise(game_count = sum(game_count), .groups = "drop")
@@ -436,7 +377,6 @@ server <- function(input, output) {
       spread(key = week, value = game_count, fill = 0)
 
     data_matrix <- as.matrix(data_matrix[, -1])
-
     heatmaply(
       data_matrix,
       xlab = "Week", 
@@ -454,89 +394,51 @@ server <- function(input, output) {
   
   
   
-  dataset7 <- reactive({
+  DensityPlotsData <- reactive({
     nazwa_csv <- paste0(input$dataset4, ".csv")
     data <- read.csv(nazwa_csv)
-    
     data <- data %>%
       mutate(
         Date = format(as.POSIXct(Date), "%Y-%m-%d"),
         Day = format(as.POSIXct(Date), "%m-%d"),
         win = ifelse(Win == "True", 1, 0)
-      ) %>%
-      filter(
-        Date >= as.POSIXct(as.character(input$date_range4[1]), "%Y-%m-%d"),
-        Date <= as.POSIXct(as.character(input$date_range4[2]), "%Y-%m-%d"),
-        if (input$position4 != "All") {
-          Position == input$position4
-        } else {
-          TRUE
-        }
-      ) %>% mutate(gameLength=gameLength/60, goldPerMinute = ceiling(goldPerMinute))
+      ) %>% filter_data(input$date_range1,input$position1)%>%
+      mutate(gameLength=gameLength/60, goldPerMinute = ceiling(goldPerMinute))
     
     return(data)
   })
-  output$plot9 <- renderPlotly({
-    data <- dataset7()
-    
+  
+  output$DensityDuration <- renderPlotly({
+    data <- DensityPlotsData()
     plot9 <- ggplot(data, aes(x = gameLength)) +
       geom_density()
-    labs(
-      title = "Game duration",
-      x = "Game duration",
-      y = "Density"
-    ) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(angle = 45, size = 7), 
-        axis.title = element_text(size = 14), 
-        plot.title = element_text(size = 16)
-      )
-    
+    plot9<- add_custom_theme(plot9,"Game duration","Density","Game Duration")
     ggplotly(plot9)
   })
-  output$plot7 <- renderPlotly({
-    data <- dataset7()
+  
+  output$DensityGold<- renderPlotly({
+    data <- DensityPlotsData()
     
     plot7 <- ggplot(data, aes(x = goldPerMinute)) +
       geom_density()
-    labs(
-      title = "Gold per minute",
-      x = "Gold per minute",
-      y = "Density"
-    ) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(angle = 45, size = 7), 
-        axis.title = element_text(size = 14), 
-        plot.title = element_text(size = 16)
-      )
-    
+    plot7<- add_custom_theme(plot7,"Gold per minute","Density","Gold per minute")
     ggplotly(plot7)
   })
-  output$plot8 <- renderPlotly({
-    data <- dataset7()
-    
+  
+  output$DensityDamage <- renderPlotly({
+    data <- DensityPlotsData()
     plot8 <- ggplot(data, aes(x = damagePerMinute)) +
       geom_density()
-    labs(
-      title = "Damage per minute",
-      x = "Damage per minute",
-      y = "Density"
-    ) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(angle = 45, size = 7), 
-        axis.title = element_text(size = 14), 
-        plot.title = element_text(size = 16)
-      )
-    
+    plot8<- add_custom_theme(plot8,"Damage per minute","Density","Damage per minute")
     ggplotly(plot8)
   })
   
 }
 
 shinyApp(ui = ui, server = server)
+
+
+
 
 
 
